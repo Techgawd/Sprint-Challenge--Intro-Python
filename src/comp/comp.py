c7ef7bff2f878238fe8719fd3e60d76a25ec65a4
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-06-15 17:10:46 -0700

    Finished 2 more exercises in comp for MVP

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -55,7 +55,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(x.name, x.age) for x in humans if x.age >= 27 and x.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -68,5 +68,5 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(x.age) for x in humans]
 print(h)
